a1c8932b6153a21d16b6d968cbf401760bfe95fa
yash-surviantllc <yash@surviant.in>
2026-07-15 17:26:24 +0530

    navbar , login , signup

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login, updateProfile, logout, refreshToken } from "../controllers/user.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload, register);
 router.route("/login").post(login);
 router.route("/logout").get(logout).post(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile); // we are using isAuthenticated middleware to check if the user is authenticated or not before updating the profile
---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <div className='text-centre'>
+            <span>
+                <h2 className='px-4 py-2 rounded-full bg-gray-100 font-medium'>Best Job Hunting Site</h2>
+            </span>
+            <h1 className='text-5xl font-bold'>Search , Apply & Get Your Dream Job</h1>
+        </div>
+    )
+}
+
+export default HeroSection
