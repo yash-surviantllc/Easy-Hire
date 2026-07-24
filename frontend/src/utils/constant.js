23ed6cec37cc55a5d13cd2326cb444094de2b0fb
yash-surviantllc <yash@surviant.in>
2026-07-24 14:50:42 +0530

    frontend and backend URL constants changes for deployment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+export const USER_API_END_POINT = `${API_BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${API_BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${API_BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${API_BASE_URL}/api/v1/company`;
